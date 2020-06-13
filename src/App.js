9cfcf57ecb08420aae8ae0ccdbedf19dbcba97ee
兔子先生 <1993iweb@gmail.com>
2020-06-13 15:43:39 +0800

    试试会不会

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        自动构建
+        自动构建   再试试
         </p>
         <a
           className="App-link"
